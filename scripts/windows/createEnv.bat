fa2dcfe750633a9fb2b3bb756d49f9fb3f316fb9
Pier4413 <pierre.delmas@delmasweb.net>
2022-02-22 22:54:37 +0100

    Ajout de la locale dans les traductions

--- scripts/windows/createEnv.bat
+++ scripts/windows/createEnv.bat
@@ -4,4 +4,4 @@ echo [Database]>conf/settings.ini
 echo filename=comptes.db>>conf/settings.ini
 echo [Translation]>>conf/settings.ini
 echo folder_path=./resources/translation>>conf/settings.ini
-echo locale=fr
+echo locale=fr>>conf/settings.ini
