aca7d3d8aa88fa4ba338f60f58d04e4c770a06f4
Pier4413 <pierre.delmas@delmasweb.net>
2022-02-22 21:40:54 +0100

    Correction d'un test

--- tests/integration/compte.py
+++ tests/integration/compte.py
@@ -8,7 +8,7 @@ from classes.elements.compte import Compte as CompteModele
 from classes.sql.compte import Compte as CompteSQL
 
 if __name__ == "__main__":
-    compte_sql = CompteSQL("comptes.sql")
+    compte_sql = CompteSQL("comptes.db")
 
     # Test de creation de la table
     try:
