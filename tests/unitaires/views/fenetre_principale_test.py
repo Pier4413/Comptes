d9778efb73b29d862fc564f8dd700fabb7110f8a
Pier4413 <pierre.delmas@delmasweb.net>
2022-02-24 12:35:15 +0100

    Suppression des tests unitaires sur l'IHM inutiles

--- tests/unitaires/views/fenetre_principale_test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import pytestqt
-
-from views.fenetre_principale import FenetrePrincipale
-
-@pytest.mark.skip("Non testee car rien a tester")
-class TestFenetrePrincipale():
-    pass
